20398d720a26d7495048afebabe3c2aa1760df06
gsbenegas <gsbenegas>
2014-06-27 04:23:55 +0000

    no lo miren

--- tp3/src/scripts/gen_test_plot_local.py
+++ tp3/src/scripts/gen_test_plot_local.py
@@ -6,11 +6,11 @@ os.system("tar czf ../backup/local_search_%s.tar.gz ../input ../output/local_sea
 os.system("rm ../input/*")
 os.system("rm ../output/local_search/*")
 
-for n in range(10, 30):
+for n in range(5, 110):
     m = (n*(n-1))/4
     #m = int(math.sqrt(n))*n
     #m = 10*n
-    subprocess.call(["python", "createInput_magic.py", "100", str(n), str(m)])
+    subprocess.call(["python", "createInput_magic.py", "20", str(n), str(m)])
 
 subprocess.call(["python3", "correrTestTiempo.py", "local_search", "magic"])
 subprocess.call(["python", "graficar_local_search.py"])
